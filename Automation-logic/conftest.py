8b34c2140f57f85f42bbd8255661347c69a77092
Rutwik-Internetsoft <rutvik@internetsoft.site>
2025-03-19 12:32:25 +0530

    Update conftest.py

--- Automation-logic/conftest.py
+++ Automation-logic/conftest.py
@@ -20,7 +20,7 @@ if project_root not in sys.path:
 class AppiumDriver:
     def __init__(self):
         self.driver = None
-        self.url = "http://192.168.56.1:4723"
+        self.url = "http://127.0.0.1:4723/"
         self.caps: Dict[str, Any] = {
             "platformName": "Android",
             "appium:deviceName": "TS43223941452",
